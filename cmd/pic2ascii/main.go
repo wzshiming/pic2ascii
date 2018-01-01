5f0f683a396c3cbda61e5919b4292dbf21ca1a88
wzshiming <wzshiming@foxmail.com>
2018-01-01 18:55:14 +0800

    add get http and add git

--- cmd/pic2ascii/main.go
+++ cmd/pic2ascii/main.go
@@ -5,9 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"image"
+	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 	"io/ioutil"
+	"net/http"
+	"net/url"
 
 	"github.com/nfnt/resize"
 	"github.com/wzshiming/pic2ascii"
@@ -15,16 +18,48 @@ import (
 )
 
 func main() {
-	pic := flag.String("pic", "", "image file")
-	chars := flag.String("chars", `MNXKAVQOCL#kxdoclc\=;:"'. `, "chars")
-	w := flag.Uint("width", 0, "resize width")
-	h := flag.Uint("height", 0, "resize height")
-	o := flag.String("out", "", "out file")
+	pic := flag.String("p", "", "input image file")
+	chars := flag.String("c", `MNXKAVQOCL#kxdoclc\=;:"'. `, "chars")
+	w := flag.Uint("w", 0, "resize width")
+	h := flag.Uint("h", 0, "resize height")
+	o := flag.String("o", "", "output file")
 	flag.Parse()
 
-	f, err := ioutil.ReadFile(*pic)
+	if *pic == "" {
+		flag.Usage()
+		return
+	}
+
+	u, err := url.Parse(*pic)
 	if err != nil {
 		ffmt.Mark(err)
+		return
+	}
+
+	var f []byte
+
+	switch u.Scheme {
+	case "http", "https":
+		resp, err := http.Get(u.String())
+		if err != nil {
+			ffmt.Mark(err)
+			return
+		}
+		defer resp.Body.Close()
+
+		f, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			ffmt.Mark(err)
+			return
+		}
+	case "file", "":
+		f, err = ioutil.ReadFile(u.Path)
+		if err != nil {
+			ffmt.Mark(err)
+			return
+		}
+	default:
+		ffmt.Mark("unknown scheme ", u.Scheme)
 		return
 	}
 
