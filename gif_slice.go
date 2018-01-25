411a08b245d40f60289d1356398f5a401c203f3c
wzshiming <wzshiming@foxmail.com>
2018-01-25 15:02:10 +0800

    Optimize

--- gif_slice.go
+++ gif_slice.go
@@ -8,20 +8,25 @@ import (
 
 // SliceGIF
 func SliceGIF(g *gif.GIF) (r []image.Image) {
-	for _, v := range g.Image {
-		if len(r) == 0 {
-			r = append(r, v)
-		} else {
-			r = append(r, MergeImage(v, r[len(r)-1]))
-		}
+	if len(g.Image) == 0 {
+		return nil
 	}
-	return
+	r = make([]image.Image, 0, len(g.Image))
+	r = append(r, g.Image[0])
+	if len(g.Image) == 1 {
+		return r
+	}
+
+	for _, v := range g.Image[1:] {
+		r = append(r, MergeImage(r[len(r)-1], v))
+	}
+	return r
 }
 
 // MergeImage
 func MergeImage(src image.Image, over image.Image) image.Image {
-	dist := image.NewNRGBA(over.Bounds())
-	draw.Draw(dist, dist.Bounds(), over, image.ZP, draw.Src)
-	draw.Draw(dist, dist.Bounds(), src, image.ZP, draw.Over)
+	dist := image.NewRGBA(src.Bounds())
+	draw.Draw(dist, dist.Bounds(), src, image.ZP, draw.Src)
+	draw.Draw(dist, dist.Bounds(), over, image.ZP, draw.Over)
 	return dist
 }
