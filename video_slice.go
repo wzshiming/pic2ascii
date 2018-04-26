6c0dd4c8975af4c55d19b7d36977876e0008d919
wzshiming <wzshiming@foxmail.com>
2018-04-27 06:20:00 +0800

    video seek buffer

--- video_slice.go
+++ video_slice.go
@@ -3,10 +3,9 @@
 package pic2ascii
 
 import (
-	"bytes"
+	"errors"
 	"image"
 	"io"
-	"io/ioutil"
 	"time"
 
 	"github.com/nareix/joy4/av/avutil"
@@ -18,11 +17,56 @@ var rc io.Reader
 var iotoken = "mem://video"
 
 // 兼容 github.com/nareix/joy4 一个强制断言的 bug
+
 type noper struct {
-	io.ReadSeeker
+	r io.Reader
+	s []byte
+	i int64
 }
 
-func (noper) Close() error { return nil }
+func (r *noper) sycn(i int) (int64, error) {
+	return io.CopyN(r, r.r, int64(i))
+}
+
+func (r *noper) Write(p []byte) (n int, err error) {
+	r.s = append(r.s, p...)
+	return len(p), nil
+}
+
+func (r *noper) Read(b []byte) (n int, err error) {
+	if r.i >= int64(len(r.s)) {
+		if i, _ := r.sycn(1024 * 10); i != 0 {
+			return r.Read(b)
+		}
+		return 0, io.EOF
+	}
+	n = copy(b, r.s[r.i:])
+	r.i += int64(n)
+	return
+}
+
+func (r *noper) Seek(offset int64, whence int) (int64, error) {
+	var abs int64
+	switch whence {
+	case io.SeekStart:
+		abs = offset
+	case io.SeekCurrent:
+		abs = r.i + offset
+		//	case io.SeekEnd:
+		//		abs = int64(len(r.s)) + offset
+	default:
+		return 0, errors.New("pic2ascii.noper.Seek: invalid whence")
+	}
+	if abs < 0 {
+		return 0, errors.New("pic2ascii.noper.Seek: negative position")
+	}
+	r.i = abs
+	return abs, nil
+}
+
+func (r *noper) Close() error { return nil }
+
+func newNoper(r io.Reader) *noper { return &noper{r, nil, 0} }
 
 func init() {
 	avutil.DefaultHandlers.Add(func(r *avutil.RegisterHandler) {
@@ -30,14 +74,10 @@ func init() {
 			if f != iotoken {
 				return false, nil, nil
 			}
-			data, err := ioutil.ReadAll(rc)
-			if err != nil {
-				return false, nil, err
-			}
 
 			if rc0 := rc; rc0 != nil {
 				rc = nil
-				return true, noper{bytes.NewReader(data)}, nil
+				return true, newNoper(rc0), nil
 			}
 			return false, nil, nil
 		}
@@ -72,7 +112,6 @@ func VideoSlice(read io.Reader, f func(time.Duration, image.Image)) error {
 		}
 	}
 
-	//	buf := bytes.NewBuffer(nil)
 	for {
 		pkt, err := file.ReadPacket()
 		if err != nil {
