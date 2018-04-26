ec7f116ec0b495bdde3a6f797bdfa4d41f1b0b29
wzshiming <wzshiming@foxmail.com>
2018-04-27 03:45:21 +0800

    update travis

--- build_all.bash
+++ build_all.bash
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-BASENAME=pic2ascii
 SRC_ROOT=$(git rev-parse --show-toplevel)
 VERSION=$(git describe --tags --dirty)
 COMMIT_HASH=$(git rev-parse --short HEAD 2>/dev/null)
 DATE=$(date "+%Y-%m-%d")
 IMPORT_DURING_SOLVE=${IMPORT_DURING_SOLVE:-false}
+
+if [[ -z "${BASENAME}" ]]; then
+    BASENAME="out"
+fi
+
+if [[ -z "${RELEASE}" ]]; then
+    RELEASE="release"
+fi
 
 if [[ "$(pwd)" != "${SRC_ROOT}" ]]; then
   echo "you are not in the root of the repo" 1>&2
@@ -14,7 +21,11 @@ if [[ "$(pwd)" != "${SRC_ROOT}" ]]; then
   exit 1
 fi
 
-GO_BUILD_CMD="go build -a -installsuffix cgo"
+if [[ -z "${GO_BUILD_FLAGS}" ]]; then
+    GO_BUILD_FLAGS="-a -installsuffix cgo"
+fi
+
+GO_BUILD_CMD="go build ${GO_BUILD_FLAGS} "
 GO_BUILD_LDFLAGS="-s -w -X main.commitHash=${COMMIT_HASH} -X main.buildDate=${DATE} -X main.version=${VERSION} -X main.flagImportDuringSolve=${IMPORT_DURING_SOLVE}"
 
 if [[ -z "${SRC_BUILD_PLATFORMS}" ]]; then
@@ -25,7 +36,7 @@ if [[ -z "${SRC_BUILD_ARCHS}" ]]; then
     SRC_BUILD_ARCHS="amd64 386"
 fi
 
-mkdir -p "${SRC_ROOT}/release"
+mkdir -p "${SRC_ROOT}/${RELEASE}"
 
 for OS in ${SRC_BUILD_PLATFORMS[@]}; do
   for ARCH in ${SRC_BUILD_ARCHS[@]}; do
@@ -33,9 +44,9 @@ for OS in ${SRC_BUILD_PLATFORMS[@]}; do
     if [[ "${OS}" == "windows" ]]; then
       NAME="${NAME}.exe"
     fi
-    echo "Building for ${OS}/${ARCH}"
-    GOARCH=${ARCH} GOOS=${OS} CGO_ENABLED=0 ${GO_BUILD_CMD} -ldflags "${GO_BUILD_LDFLAGS}"\
-     -o "${SRC_ROOT}/release/${NAME}" ./cmd/${BASENAME}
-    shasum -a 256 "${SRC_ROOT}/release/${NAME}" > "${SRC_ROOT}/release/${NAME}".sha256
+    NAME="${SRC_ROOT}/${RELEASE}/${NAME}"
+    echo "Building to ${NAME} for ${OS}/${ARCH}"
+    GOARCH=${ARCH} GOOS=${OS} CGO_ENABLED=0 ${GO_BUILD_CMD} -ldflags "${GO_BUILD_LDFLAGS}" -o "${NAME}" ./cmd/${BASENAME}
+    shasum -a 256 "${NAME}" > "${NAME}".sha256
   done
 done
